6df979185635dde82161270f82b0010ed848bb06
Guilherme Ferreira <guilherme.fs.db@gmail.com>
2023-09-14 12:52:14 -0300

    Add files via upload

---
+++ TP02-MVC/Models/Bl.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP02_MVC.Models
+{
+    [Table("bls")]
+    public class Bl
+    {
+        [Display(Name = "Código")]
+        [Column("id")]
+        public int Id { get; set; } 
+
+        [Display(Name = "Consignee")]
+        [Column("consignee")]
+        public string Consignee { get; set; } 
+
+        [Display(Name = "Navio")]
+        [Column("ship")]
+        public int Ship { get; set; } 
+
+        public virtual ICollection<Container>? Containers { get; set; }
+    }
+}
---
+++ TP02-MVC/Models/Container.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TP02_MVC.Models
+{
+    public class Container
+    {
+        [Display(Name = "Código")]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo")]
+        [Column("type")]
+        public string Tipo { get; set; }
+
+        [Display(Name = "Tamanho")]
+        [Column("size")]
+        public string Tamanho { get; set; }
+
+        [Column("bl_id")]
+        public int BlId { get; set; }
+
+        [ForeignKey("BlId")]
+        public Bl? Bl { get; set; }
+    }
+}
